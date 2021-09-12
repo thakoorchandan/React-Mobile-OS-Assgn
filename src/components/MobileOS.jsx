2d5b5c5c84a3e6e9aa9b758b0768f599fad6618d
Thakoor Chandan <thakoorchandan@gmail.com>
2021-09-12 15:47:18 +0530

    Pracetice Assignment

---
+++ src/components/MobileOS.jsx
@@ -0,0 +1,15 @@
+function Mobileos(){
+    return(
+        <>
+        <h1>Mobile Operating System</h1>
+        <ul>
+        <li>Android</li>
+        <li>BlackBerry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+        </ul>
+       </>
+    );
+}
+
+export default Mobileos;
---
+++ src/components/Mobileman.jsx
@@ -0,0 +1,15 @@
+function Mobileman(){
+    return(
+        <>
+        <h1>Moble Manufacturers</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+        </>
+    );
+}
+
+export default Mobileman;
